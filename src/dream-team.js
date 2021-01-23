23ee0b8cef4e099dba4f1d77a767f26ca9984035
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-23 15:54:23 +0500

    feat: add isArray checking

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false
+  }
+
   let res = []
   let withOutSpaces = ''
 
   for(let i = 0; i < members.length; i++) {
-    if(typeof members[i] === "string") {
+    if(typeof members[i] == "string") {
       withOutSpaces = members[i].trim()
       res.push(withOutSpaces[0].toUpperCase())
     }
   }
 
-  if(res.length == 0) {
-    return false
-  }
 
   return res.sort().join('')
 }
