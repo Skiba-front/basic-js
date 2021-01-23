4208146c67a8c028003e8957b5d2a64a57305f5b
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-23 15:44:13 +0500

    feat: complete task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let res = []
+  let withOutSpaces = ''
+
+  for(let i = 0; i < members.length; i++) {
+    if(typeof members[i] == "string") {
+      withOutSpaces = members[i].trim()
+      res.push(withOutSpaces[0].toUpperCase())
+    }
+  }
+
+  if(res.length == 0) {
+    return false
+  }
+
+  return res.sort().join('')
+}
