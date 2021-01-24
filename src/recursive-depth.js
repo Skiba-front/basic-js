f87464c40d3571607b83b944da879f4da327aaf6
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-24 19:08:44 +0500

    feat: complite task (no)

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,13 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 let count = 1
 module.exports = class DepthCalculator {
+  constructor() {
+    this.count = 1;
+  }
+  
   calculateDepth(arr) {
   for(let i = 0; i < arr.length; i++) {
     if(Array.isArray(arr[i])) {
-      count++
-      calculateDepth(arr[i])
+      this.count++
+      this.calculateDepth(arr[i])
     }
   }
-  return count
+  return this.count
 }
 };
