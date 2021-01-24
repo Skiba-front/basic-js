1a021b2f2b9bbebb38d2d5a63b877237020b4eeb
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-24 18:50:38 +0500

    feat: complite task first try

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,13 @@
 const CustomError = require("../extensions/custom-error");
-
+let count = 1
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+      count++
+      calculateDepth(arr[i])
+    }
   }
+  return count
+}
 };
