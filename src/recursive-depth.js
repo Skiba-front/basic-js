8711ad611a308d38ca10dc35089a520e56704845
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-27 15:55:07 +0500

    feat: complete task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,19 +2,18 @@ const CustomError = require("../extensions/custom-error");
 let count = 1
 module.exports = class DepthCalculator {
   
-  calculateDepth(arr, depth = 0, a = 0 ) {
-
+  calculateDepth(arr, count = 0, curCount = 0 ) {
     if (Array.isArray(arr)) {
       for (let i = 0; i < arr.length; i++) {
 
-        a = this.calculateDepth(arr[i]);
+        curCount = this.calculateDepth(arr[i]);
 
-        if (a > depth) {
-          depth = a;
+        if (curCount > count) {
+          count = curCount;
         }
       }
-      return depth + 1;
-    }else{
+      return count + 1;
+    } else {
       return 0;
     }
 
