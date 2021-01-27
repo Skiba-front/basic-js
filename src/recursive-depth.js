05a4aefda236ef77c80aab6642bd14f05eb766f3
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-27 15:47:05 +0500

    feat: a tak

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 let count = 1
 module.exports = class DepthCalculator {
-  constructor() {
-    this.count = 1;
-  }
   
-  calculateDepth(arr) {
-  for(let i = 0; i < arr.length; i++) {
-    if(Array.isArray(arr[i])) {
-      this.count++
-      this.calculateDepth(arr[i])
+  calculateDepth(arr, depth = 0, a = 0 ) {
+
+    if (Array.isArray(arr)) {
+      for (let i = 0; i < arr.length; i++) {
+
+        a = this.calculateDepth(arr[i]);
+
+        if (a > depth) {
+          depth = a;
+        }
+      }
+      return depth + 1;
+    }else{
+      return 0;
     }
+
   }
-  return this.count
-}
 };
