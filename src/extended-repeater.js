f58464df9a2a044dd62f67c631c8a4f57a901d90
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-02-01 00:17:24 +0500

    feat: complete teask(no)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  str = String(str)
+  options.addition = String(options.addition) !== 'undefined' ? String(options.addition) : ''
+  options.separator = options.separator ? options.separator : '+'
+  options.additionSeparator = options.additionSeparator ? options.additionSeparator : '|'
+  
+  let res = ''
+  let dop = ''
+  if (options.additionRepeatTimes) {
+   for (let i = 1; i < options.additionRepeatTimes; i++) {
+    dop += options.addition + options.additionSeparator
+   } 
+  } 
+  dop += options.addition
+
+  if (options.repeatTimes) {
+    for (let i = 1; i <= options.repeatTimes; i++) {
+      res += str + dop
+      if (!(i === options.repeatTimes)) {
+        res += options.separator
+      }
+    } 
+  } else {
+    res = str + dop
+  }
+
+  return res
+}
   
