fce2eb63917a2aca9c6193ac4eec98fa41adaca0
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-01-31 16:44:24 +0500

    fix: some problems

--- src/what-season.js
+++ src/what-season.js
@@ -2,9 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
 
-  if(isNaN(date.valueOf())) {
+  if(date === undefined) {
     return 'Unable to determine the time of year!'
-  } else if(!(date instanceof Date) || typeof(+date) !== 'number') {
+  } else if(isNaN(Date.parse(date)) || typeof(+date) !== 'number') {
     throw new CustomError('Not implemented');
   }
 
